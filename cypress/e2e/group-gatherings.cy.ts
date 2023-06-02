ce994cd1fdbc549eb9f14645036fd021f07ec7e2
Philip Carlsson <philipcarlsson@hotmail.com>
2023-06-02 14:08:59 +0200

    writing smoke test

--- cypress/e2e/group-gatherings.cy.ts
+++ cypress/e2e/group-gatherings.cy.ts
@@ -1,12 +1,23 @@
 describe('Group gatherings', () => {
   it('User can find an event', () => {
     cy.visit('/');
-    Cypress.on('uncaught:exception', (err, runnable) => {
+    /*   Cypress.on('uncaught:exception', (err, runnable) => {
       return false;
-    });
+    }); */
     //go to group page from home
+    /* cy.findByRole('button', { name: /groups/i }).click(); */
+
+    cy.contains(/groups/i).click();
     //Click on group "GroupOnlyForUnitTest"
+    cy.contains(/GroupOnlyForUnitTest/i).click();
+
     //Select "alla sammankomster"
+    cy.contains(/all events/i).click();
+
     //Press "EventUnitTest2" should now be on /events/-67
+    cy.contains(/EventUnitTest2/i).click();
+
+    //Cointains member list
+    cy.contains(/Member/i).should('be.visible');
   });
 });
